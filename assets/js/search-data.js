b8c360e30713b3ebe10c1713db66ec1cae4da58a
stephenchen06 <stephc@ad.unc.edu>
2026-04-12 05:34:14 +0000

    Deploying to gh-pages from @ stephenchen06/stephenchen06.github.io@649e9cc58841d776a4a45000584b575a7847f997 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -400,12 +400,7 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "books-computational-and-inferential-thinking",
+      },{id: "books-computational-and-inferential-thinking",
           title: 'Computational and Inferential Thinking',
           description: "",
           section: "Books",handler: () => {
